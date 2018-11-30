fcf0c86b6023d8ae0b880b283b953dc352cb9e3f
Rafael G. Santana <rafaelgoncalvessantana@gmail.com>
2018-11-30 19:07:35 -0200

    Atualizando arquivos

---
+++ header.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Header</title>
+  </head>
+  <body>
+    <main>
+      <div class="page-loader">
+        <div class="loader">Loading...</div>
+      </div>
+      <nav class="navbar navbar-custom navbar-fixed-top navbar-transparent" role="navigation">
+        <div class="container">
+          <div class="navbar-header">
+            <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#custom-collapse"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button><a class="navbar-brand" href="index.html"><img class="logoPg" src="assets/images/shop/logo.png" width="40px"/> POLTRONA <span class="logoGeek">GEEK</span></a>
+          </div>
+          <div class="collapse navbar-collapse" id="custom-collapse">
+            <ul class="nav navbar-nav navbar-right">
+              <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">LIVROS</a>
+                <ul class="dropdown-menu">
+                  <li><a href="index_mp_fullscreen_video_background.html">DC COMICS</a></li>
+                  <li><a href="index_op_fullscreen_gradient_overlay.html">MARVEL</a></li>
+                  <li><a href="index_agency.html">MANGÁS</a></li>
+                  <li><a href="index_portfolio.html">Portfolio</a></li>
+                  <li><a href="index_restaurant.html">Restaurant</a></li>
+                  <li><a href="index_finance.html">Finance</a></li>
+                  <li><a href="index_landing.html">Landing Page</a></li>
+                  <li><a href="index_photography.html">Photography</a></li>
+                  <li><a href="index_shop.html">Shop</a></li>
+                </ul>
+              </li>
+              <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">GAMES</a>
+                <ul class="dropdown-menu">
+                  <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Static Image Header</a>
+                    <ul class="dropdown-menu">
+                      <li><a href="index_mp_fullscreen_static.html">Fulscreen</a></li>
+                      <li><a href="index_mp_classic_static.html">Classic</a></li>
+                    </ul>
+                  </li>
+                  <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Flexslider Header</a>
+                    <ul class="dropdown-menu">
+                      <li><a href="index_mp_fullscreen_flexslider.html">Fulscreen</a></li>
+                      <li><a href="index_mp_classic_flexslider.html">Classic</a></li>
+                    </ul>
+                  </li>
+                  <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Video Background Header</a>
+                    <ul class="dropdown-menu">
+                      <li><a href="index_mp_fullscreen_video_background.html">Fulscreen</a></li>
+                      <li><a href="index_mp_classic_video_background.html">Classic</a></li>
+                    </ul>
+                  </li>
+                  <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Text Rotator Header</a>
+                    <ul class="dropdown-menu">
+                      <li><a href="index_mp_fullscreen_text_rotator.html">Fulscreen</a></li>
+                      <li><a href="index_mp_classic_text_rotator.html">Classic</a></li>
+                    </ul>
+                  </li>
+                  <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Gradient Overlay Header</a>
+                    <ul class="dropdown-menu">
+                      <li><a href="index_mp_fullscreen_gradient_overlay.html">Fulscreen</a></li>
+                      <li><a href="index_mp_classic_gradient_overlay.html">Classic</a></li>
+                    </ul>
+                  </li>
+                </ul>
+              </li>
+              <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">VINIS</a>
+                <ul class="dropdown-menu">
+                  <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">About</a>
+                    <ul class="dropdown-menu">
+                      <li><a href="about1.html">About 1</a></li>
+                      <li><a href="about2.html">About 2</a></li>
+                      <li><a href="about3.html">About 3</a></li>
+                      <li><a href="about4.html">About 4</a></li>
+                      <li><a href="about5.html">About 5</a></li>
+                    </ul>
+                  </li>
+            <!--      <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">SERVIÇOS NERDS</a>
+                    <ul class="dropdown-menu">
+                      <li><a href="service1.html">SUA FANTASIA EM 1H</a></li>
+                      <li><a href="service2.html">INGRESSO RÁPIDO</a></li>
+                      <li><a href="service3.html">DISPUTA DE YU-GI-OH</a></li>
+                    </ul>
+                  </li> -->
+              <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Pricing</a>
+                    <ul class="dropdown-menu">
+                      <li><a href="pricing1.html">Pricing 1</a></li>
+                      <li><a href="pricing2.html">Pricing 2</a></li>
+                    </ul>
+                  </li>
+                  <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Gallery</a>
+                    <ul class="dropdown-menu">
+                      <li><a href="gallery_col_2.html">2 Columns</a></li>
+                      <li><a href="gallery_col_3.html">3 Columns</a></li>
+                      <li><a href="gallery_col_4.html">4 Columns</a></li>
+                      <li><a href="gallery_col_6.html">6 Columns</a></li>
+                    </ul>
+                  </li>
+                  <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Contact</a>
+                    <ul class="dropdown-menu">
+                      <li><a href="contact1.html">Contact 1</a></li>
+                      <li><a href="contact2.html">Contact 2</a></li>
+                      <li><a href="contact3.html">Contact 3</a></li>
+                    </ul>
+                  </li>
+                  <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Restaurant menu</a>
+                    <ul class="dropdown-menu">
+                      <li><a href="restaurant_menu1.html">Menu 2 Columns</a></li>
+                      <li><a href="restaurant_menu2.html">Menu 3 Columns</a></li>
+                    </ul>
+                  </li>
+                  <li><a href="login_register.html">Login / Register</a></li>
+                  <li><a href="faq.html">FAQ</a></li>
+                  <li><a href="404.html">Page 404</a></li>
+                </ul>
+              </li>
+              <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">TOYS</a>
+                <ul class="dropdown-menu" role="menu">
+                  <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">SUA FANTASIA EM 1H</a>
+                    <ul class="dropdown-menu">
+                      <li><a href="portfolio_boxed_col_2.html">2 Columns</a></li>
+                      <li><a href="portfolio_boxed_col_3.html">3 Columns</a></li>
+                      <li><a href="portfolio_boxed_col_4.html">4 Columns</a></li>
+                    </ul>
+                  </li>
+                  <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">INGRESSO RÁPIDO</a>
+                    <ul class="dropdown-menu">
+                      <li><a href="portfolio_boxed_gutter_col_2.html">2 Columns</a></li>
+                      <li><a href="portfolio_boxed_gutter_col_3.html">3 Columns</a></li>
+                      <li><a href="portfolio_boxed_gutter_col_4.html">4 Columns</a></li>
+                    </ul>
+                  </li>
+                  <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">DISPUTA DE YU-GI-OH</a>
+                    <ul class="dropdown-menu">
+                      <li><a href="portfolio_full_width_col_2.html">2 Columns</a></li>
+                      <li><a href="portfolio_full_width_col_3.html">3 Columns</a></li>
+                      <li><a href="portfolio_full_width_col_4.html">4 Columns</a></li>
+                    </ul>
+                  </li>
+                  <div class="container">
+                    <div class="carrinho">
+
+                    </div>
+                    <div class="login">
+
+                    </div>
+                  </div>
+      </nav>
+  </body>
+</html>
